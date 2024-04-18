39bce6d3144163b0fde67fc82897e81b8b5a1c22
Guilherme <guilhermesantosalves068@gmail.com>
2024-04-18 14:13:09 -0300

    Home de patient atualizada

--- js/patient.js
+++ js/patient.js
@@ -1,18 +1,20 @@
-function changeGrid() {
-  var element = document.querySelector(".main-content");
-  element.style.gridTemplateColumns = "290px 0px auto";
-  var center = document.querySelector(".center");
-  center.innerHTML = '';
-}
-
-function apagar(){
-  var center = document.querySelector(".center");
-  center.innerHTML = '';
-  let contentConsultations = document.querySelector(".content-consultations");
-  contentConsultations.innerHTML = '';
-  let contentExams = document.querySelector(".content-exams");
-  contentExams.innerHTML = '';
-}
+window.addEventListener('scroll', function() {
+  var sidebar = document.querySelector('.cl-sidebar');
+  var alturaViewport = window.innerHeight;
+  var scrollTop = window.scrollY;
+  var alturaTotal = Math.max(document.body.scrollHeight, document.body.offsetHeight, 
+                             document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+  
+  var alturaRestante = alturaTotal - (alturaViewport + scrollTop);
+  
+  if (alturaRestante <= 0) {
+      // Se não houver mais espaço para rolar, ajusta a altura da barra lateral para a altura total da página
+      sidebar.style.height = alturaTotal + 'px';
+  } else {
+      // Caso contrário, ajusta a altura da barra lateral para a altura da viewport
+      sidebar.style.height = alturaViewport + 'px';
+  }
+});
 
 let successMsg = '<span class="material-symbols-outlined">check_circle</span>Sucesso';
 let errorMsg = '<span class="material-symbols-outlined">cancel</span>Erro';
@@ -123,97 +125,33 @@ function checkModality() {
 }
 
 function buildInputsExams() {
-  let contentConsultations = document.querySelector(".content-consultations");
+  let contentConsultations = document.querySelector(".cl-consultations");
   contentConsultations.innerHTML = '';
-  let contentExams = document.querySelector(".content-exams");
+  let contentExams = document.querySelector(".cl-exams");
   contentExams.innerHTML = '';
 
   // Template do formulário de exames
   const templateExams = `
-    <h1 class="title">Agendar Exame</h1>
-    <form action="javascript:void(0)" class="form-box">
-        <div class="specialty" id="specialty">
-            <div class="select-custom -exam-or-consultation" id="select-custom">
-                <label class="select-speciality" for="specialty-exam">Selecione a especialidade do exame:</label>
-                <select required name="specialty" id="specialty-exam">
-                    <option value="" disabled selected>Especialidade</option> 
-                    <option value="Exames de imagem">Exames de imagem</option>
-                    <option value="Cardiologia">Cardiologia</option>
-                </select>
-                <div class="custom-arrow">
-                    <span class="material-symbols-outlined">
-                        arrow_drop_down
-                    </span>
-                </div>
-            </div>
-        </div>
-        <div class="select-custom -exam-or-consultation">
-          <div class="doctor-select">
-            <label for="doctors-list">Médico:</label>
-            <select name="doctors-list" id="doctors-list">
-            <option value="" disabled selected>Selecione um médico</option>
-            </select> 
-            <div class="custom-arrow">
-            <span class="material-symbols-outlined">
-                arrow_drop_down
-            </span>
-            </div>
-          </div>
-        </div>
-        <div class="exam">
-            <label class="select-speciality" for="exam">Exame:</label>
-            <input class="input" required placeholder="Digite o nome do exame">
-        </div>   
-        <div class="from-the-hour">
-            <label class="between" for="exams-from-hour">Entre:</label> 
-            <input required class="input" type="time" id="exams-from-hour" value="00:00">
-            <label class="e" for="exams-until-hour">e</label> 
-            <input required class="input" type="time" id="exams-until-hour" value="23:59">
-        </div>
-        <div class="radio-container">
-            <span class="material-symbols-outlined">phone_iphone</span>
-            <label for="radio-virtual-exams">Telemedicina</label>
-            <input required name="modality" class="radio" type="radio" id="radio-virtual-exams">
-            <span class="material-symbols-outlined">person</span>
-            <label for="radio-presential-exams">Presencial</label>
-            <input required name="modality" class="radio" type="radio" id="radio-presential-exams">
-        </div>
-        <div class="cl-button -patient">
-            <button type="submit" class="btn-link" id="btn-link-exam" >Continuar</button>
-        </div>
-    </form>
-  `;
-  listDoctorsSelect();
-  // Inserir o formulário na seção de exames
-  document.querySelector(".content-exams").insertAdjacentHTML("beforeend", templateExams);  
-}
-
-function buildInputsConsultations() {
-  let contentConsultations = document.querySelector(".content-consultations");
-  contentConsultations.innerHTML = '';
-  let contentExams = document.querySelector(".content-exams");
-  contentExams.innerHTML = '';
-  
-  let templateConsultations = `
-  <h1 class="title">Agendar Consulta</h1>
+  <h1 class="title">Agendar Exame</h1>
   <form action="javascript:void(0)" class="form-box">
-    <div class="specialty" id="specialty">
-    <div class="select-custom -exam-or-consultation" id="select-custom">
-        <label class="select-speciality" for="specialty-consultation">Selecione a especialidade da consulta:</label>
-            <select name="specialty" id="specialty-consultation">
-            <option value="" disabled selected >Especialidade</option> 
-            <option value="Cardiologia">Cardiologia</option>
-            <option value="Ortopedia">Ortopedia</option>
-            </select>
-        <div class="custom-arrow">
-            <span class="material-symbols-outlined">
-                arrow_drop_down
-            </span>
-        </div>
-    </div>
-    </div>
-    <div class="select-custom -exam-or-consultation">
-        <div class="doctor-select">
+      
+      <div class="specialty" id="specialty">
+          <div class="select-custom -exam-or-consultation" id="select-custom">
+              <label class="select-speciality" for="specialty-exam">Selecione a especialidade do exame:</label>
+              <select required name="specialty" id="specialty-exam">
+                  <option value="" disabled selected>Especialidade</option> 
+                  <option value="Exames de imagem">Exames de imagem</option>
+                  <option value="Cardiologia">Cardiologia</option>
+              </select>
+              <div class="custom-arrow">
+                  <span class="material-symbols-outlined">
+                      arrow_drop_down
+                  </span>
+              </div>
+          </div>
+      </div>
+      <div class="select-custom -exam-or-consultation">
+      <div class="doctor-select">
           <label for="doctors-list">Médico:</label>
           <select name="doctors-list" id="doctors-list">
           <option value="" disabled selected>Selecione um médico</option>
@@ -223,33 +161,98 @@ function buildInputsConsultations() {
               arrow_drop_down
           </span>
           </div>
-        </div>
-    </div>
-    <div class="from-the-hour" >
-    <label class="between" for="from-date">Entre:</label>
-    <input class="input" type="time" id="consultations-from-hour" value="00:00">
-    <label class="e" for="from-date">e</label>
-    <input class="input" type="time" id="consultations-until-hour" value="23:59">
-    </div>
-    <div class="radio-container">
-    <span class="material-symbols-outlined">
-      phone_iphone
-    </span>
-    <label for="radio-virtual-consultations">Telemedicina</label><input required name="modality" class="radio" type="radio" id="radio-virtual-consultations" onchange="checkModality()">
-    <span class="material-symbols-outlined">
-      person
-    </span>
-    <label for="radio-presential-consultations">Presencial</label><input required name="modality" class="radio" type="radio" id="radio-presential-consultations" onchange="checkModality()">
-    </div>
-    <div class="cl-button -patient">
-    <button type="submit" class="btn-link" id="btn-link-consultation" >Continuar</button>
-    </div>                     
+      </div>
+      </div>
+      <div class="exam">
+          <label class="select-speciality" for="exam">Exame:</label>
+          <input class="input" required placeholder="Digite o nome do exame">
+      </div>   
+      <div class="from-the-hour">
+          <label class="between" for="exams-from-hour">Entre:</label> 
+          <input required class="input" type="time" id="exams-from-hour" value="00:00">
+          <label class="e" for="exams-until-hour">e</label> 
+          <input required class="input" type="time" id="exams-until-hour" value="23:59">
+      </div>
+      <div class="radio-container">
+          <span class="material-symbols-outlined">phone_iphone</span>
+          <label for="radio-virtual-exams">Telemedicina</label>
+          <input required name="modality" class="radio" type="radio" id="radio-virtual-exams">
+          <span class="material-symbols-outlined">person</span>
+          <label for="radio-presential-exams">Presencial</label>
+          <input required name="modality" class="radio" type="radio" id="radio-presential-exams">
+      </div>
+      <div class="cl-button -patient">
+          <button type="submit" class="btn-link" id="btn-link-exam" >Continuar</button>
+      </div>
   </form>
+  `;
+  listDoctorsSelect();
+  // Inserir o formulário na seção de exames
+  document.querySelector(".cl-exams").insertAdjacentHTML("beforeend", templateExams);  
+}
+
+function buildInputsConsultations() {
+  let contentConsultations = document.querySelector(".cl-consultations");
+  contentConsultations.innerHTML = '';
+  let contentExams = document.querySelector(".cl-exams");
+  contentExams.innerHTML = '';
+  
+  let templateConsultations = `
+  <h1 class="title">Agendar Consulta</h1>
+    <form action="javascript:void(0)" class="form-box">
+        <div class="specialty" id="specialty">
+            <div class="select-custom -exam-or-consultation" id="select-custom">
+                <label class="select-speciality" for="specialty-consultation">Selecione a especialidade da consulta:</label>
+                    <select name="specialty" id="specialty-consultation">
+                    <option value="" disabled selected >Especialidade</option> 
+                    <option value="Cardiologia">Cardiologia</option>
+                    <option value="Ortopedia">Ortopedia</option>
+                    </select>
+                <div class="custom-arrow">
+                    <span class="material-symbols-outlined">
+                        arrow_drop_down
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="select-custom -exam-or-consultation">
+        <div class="doctor-select">
+            <label for="doctors-list">Médico:</label>
+            <select name="doctors-list" id="doctors-list">
+            <option value="" disabled selected>Selecione um médico</option>
+            </select> 
+            <div class="custom-arrow">
+            <span class="material-symbols-outlined">
+                arrow_drop_down
+            </span>
+            </div>
+        </div>
+        </div>
+        <div class="from-the-hour" >
+            <label class="between" for="from-date">Entre:</label>
+            <input class="input" type="time" id="consultations-from-hour" value="00:00">
+            <label class="e" for="from-date">e</label>
+            <input class="input" type="time" id="consultations-until-hour" value="23:59">
+        </div>
+        <div class="radio-container">
+            <span class="material-symbols-outlined">
+            phone_iphone
+            </span>
+            <label for="radio-virtual-consultations">Telemedicina</label><input required name="modality" class="radio" type="radio" id="radio-virtual-consultations" onchange="checkModality()">
+            <span class="material-symbols-outlined">
+            person
+            </span>
+            <label for="radio-presential-consultations">Presencial</label><input required name="modality" class="radio" type="radio" id="radio-presential-consultations" onchange="checkModality()">
+        </div>
+        <div class="cl-button -patient">
+            <button type="submit" class="btn-link" id="btn-link-consultation" >Continuar</button>
+        </div>                     
+    </form>
   `;
 
   listDoctorsSelect();
 
-  document.querySelector(".content-consultations").insertAdjacentHTML("beforeend", templateConsultations);
+  document.querySelector(".cl-consultations").insertAdjacentHTML("beforeend", templateConsultations);
 
   const patient = document.querySelector(".left");
 
