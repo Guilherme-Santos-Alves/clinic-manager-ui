9758952ed4deea4afc52645142bd5bb6015c55d2
Guilherme <guilhermesantosalves068@gmail.com>
2024-04-27 16:18:03 -0300

    Popup de medico atualizado

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,37 @@
-function showPopup(id){
+let idAppointment;
+
+function showPopup(id) {
+    idAppointment = id;
     let popup = document.querySelector(".popup");
     let closePopup = document.querySelector(".close-popup");
 
+    popup.style.display = "none";
     popup.style.display = "block";
 
-    closePopup.addEventListener("click", function(){
+    closePopup.addEventListener("click", function() {
         popup.style.display = "none";
     });
-    alert(`${id}`);
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let btnEnter = document.querySelector("#btn-enter");
+    let btnFinish = document.querySelector("#btn-finish");
+    let btnAttachment = document.querySelector("#btn-attachment");
+
+    btnEnter.addEventListener("click", function() {
+        alert(`${idAppointment}`);
+    });
+
+    btnFinish.addEventListener("click", function() {
+        alert(`finish${idAppointment}`);
+        finishAppointment(`${idAppointment}`);
+    });
+
+    btnAttachment.addEventListener("click", function() {
+        alert(`anexo${idAppointment}`);
+    });
+});
+
+
+
+
